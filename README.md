689049b1f26ca374ca9fbac8a1031476ad370243
luffah <luffah@users.noreply.github.com>
2021-07-01 02:55:45 +0200

    add ref to chroot_start

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 ```
 MOST GAMES ON LINUX ARE BUILT FOR UBUNTU.
 THIS REPOSITORY CONTAINS LXC CONFIGURATIONS 
-THAT ALLOWS TO PLAY GAMES WITHOUT A VM.
+THAT ALLOWS TO PLAY GAMES WITH A SIMPLE LXC CONTAINER.
 ```
 Each game is associated to a directory with following structure.
+
+# Install main container
 ```
 GAMENAME/
    config
@@ -13,6 +15,16 @@ GAMENAME/
 ```
 Where GAMEDIR is empty.
 
-# Games
 
-Undertale (i386) - Work in progress
+# Install games after first config
+Use /path/to/container/$
+
+lxc-execute -n gameu -- su ubuntu -l -c "Undertale/start.sh"
+
+# Alternative to LXC
+Using LXC is a way more clean, but i made a script that doesn't require LXC and all the config part. 
+See https://github.com/luffah/u_chroot_start
+```sh
+# if the main container is in common_unprivileged and Undertale installed in adifferent home
+chroot_start.sh ../common_unprivileged/ubuntu_bionic home:/home/ubuntu /home/ubuntu "Undertale/start.sh"
+```
--- common_bionic_i386/setup.sh
+++ common_bionic_i386/setup.sh
@@ -1,45 +1,57 @@
-#!/bin/sh
-echo "= Setup user and groups for unprivileged container ="
+#!/bin/bash
+# default use current dir name as system container name
+CONTAINER_TEMPLATE_PATH="${CONTAINER_TEMPLATE_PATH:-$(dirname $0)}"
+CONTAINER_NAME=${CONTAINER_NAME:-$(basename $CONTAINER_TEMPLATE_PATH)}
+LXC_DIR="${LXC_DIR:-$HOME/.local/share/lxc}"
 
-sudo cp /etc/subuid /etc/subuid.bak
-sudo cp /etc/subgid /etc/subgid.bak
-echo "$USER:1000:1\n$USER:100000:65536" | sudo tee -a /etc/subuid /etc/subgid > /dev/null
-echo "\n-USERS IDS-"
-sort /etc/subuid | uniq | sudo tee /etc/subuid
-echo "\n-GROUPS IDS-"
-sort /etc/subgid | uniq | sudo tee /etc/subgid
+# PATH OF THE SYSTEM LXC CONTAINER
+LXC_DIR_SYSTEM="$LXC_DIR/$CONTAINER_NAME"
+# PATH OF THE GAME LXC CONTAINER
+LXC_GAME_CONTAINER_NAME="${LXC_GAME_CONTAINER_NAME:-$CONTAINER_NAME}"
+LXC_DIR_GAME="${LXC_DIR_GAME:-$HOME/$LXC_GAME_CONTAINER_NAME}"
+
+case $1)
+  user)
+    echo "= Setup user and groups for unprivileged container ="
+
+    sudo cp /etc/subuid /etc/subuid.bak
+    sudo cp /etc/subgid /etc/subgid.bak
+    echo "$USER:$UID:1\n$USER:100000:65536" | sudo tee -a /etc/subuid /etc/subgid > /dev/null
+    echo "\n-USERS IDS-"
+    sort /etc/subuid | uniq | sudo tee /etc/subuid
+    echo "\n-GROUPS IDS-"
+    sort /etc/subgid | uniq | sudo tee /etc/subgid
 
 
-if ! grep $USER /etc/lxc/lxc-usernet; then
-  echo "$USER          veth         lxcbr0     1" | sudo tee -a /etc/lxc/lxc-usernet > /dev/null
-fi
+    if ! grep $USER /etc/lxc/lxc-usernet; then
+      echo "$USER          veth         lxcbr0     1" | sudo tee -a /etc/lxc/lxc-usernet > /dev/null
+    fi
 
-mkdir -p $HOME/.local/share/lxc
-mkdir -p $HOME/.config/lxc
+    mkdir -p $HOME/.local/share/lxc
+    mkdir -p $HOME/.config/lxc
 
-if [ ! -f "$HOME/.config/lxc/default.conf" ]; then
-cat <<EOF > $HOME/.config/lxc/default.conf
+    if [ ! -f "$HOME/.config/lxc/default.conf" ]; then
+      cat <<EOF > $HOME/.config/lxc/default.conf
 lxc.net.0.type = empty
 lxc.apparmor.profile = generated
 lxc.apparmor.allow_nesting = 1
 lxc.idmap = u 0 100000 65536
 lxc.idmap = g 0 100000 65536
 EOF
-fi
-
-CONTAINER_TEMPLATE_PATH=$(dirname $0)
-CONTAINER_NAME=$(basename $CONTAINER_TEMPLATE_PATH)
-echo "\nINSTALL ${CONTAINER_NAME} IN USER SPACE ? [type 'yes' to confirm]"
-read REP
-
-[ "$REP" = "yes" ] && lxc-create -n ${CONTAINER_NAME} -t download -- --dist ubuntu --realease bionic --arch i386
-LXC_DIR="$HOME/.local/share/lxc"
-LXC_DIR_GAME="$LXC_DIR/$CONTAINER_NAME"
-LXC_DIR_CONTAINER="$HOME/$CONTAINER_NAME"
-echo "\n- Setup LXC container config="
-cd $CONTAINER_TEMPLATE_PATH
-sed "s|LXC_DIR_CONTAINER|${LXC_DIR_CONTAINER}|g;s|LXC_DIR_GAME|${LXC_DIR_GAME}|g" config.template > config
-cp config $LXC_DIR_GAME
-cp pre-start.sh $LXC_DIR_GAME
-
-echo "You have to copy the game in $LXC_DIR_GAME/home/"
+    fi
+    ;;
+  system_container)
+    echo "\nINSTALL ${CONTAINER_NAME} IN USER SPACE ? [type 'yes' to confirm]"
+    read REP
+    [ "$REP" = "yes" ] && lxc-create -n ${CONTAINER_NAME} -t download -- --dist ubuntu --realease bionic --arch i386
+    ;;
+  game_container)
+    echo "\n= Setup LXC container config="
+    cd $CONTAINER_TEMPLATE_PATH
+    sed "s|LXC_DIR_CONTAINER|${LXC_DIR_CONTAINER}|g;s|LXC_DIR_GAME|${LXC_DIR_GAME}|g;s|USER_UID|${USER_UID}|g" config.template > $LXC_DIR_GAME/config
+    sed "s|LXC_GAME_CONTAINER_NAME|${LXC_GAME_CONTAINER_NAME}|g;" start.sh > $LXC_DIR_GAME/start.sh
+    cp pre-start.sh $LXC_DIR_GAME
+    cp start.sh $LXC_DIR_GAME
+    echo "You have to copy/install the game in $LXC_DIR_GAME/home/"
+    ;;
+esac
